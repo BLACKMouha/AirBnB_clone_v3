ded7f8caa1fe99ed51d2adc1525bb64bc577b952
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-27 17:19:20 +0000

    Hashing password

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """
 Contains class BaseModel
 """
-
 from datetime import datetime
 import models
 from os import getenv
@@ -66,6 +65,8 @@ class BaseModel:
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
+        if 'password' in new_dict and models.storage_t == 'db':
+            del new_dict['password']
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         return new_dict
--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ holds class User"""
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -29,3 +30,8 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        if 'password' in kwargs:
+            passwd_bytes = kwargs['password'].encode()
+            hash_algo = haslib.md5()
+            hash_algo.update(passwd_bytes)
+            kwargs['password'] = hash_algo.digest
