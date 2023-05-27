5db38731b01389e0c7454f2aa5a5fb3079114036
BLACKMouha <mohamedsenghor05gmail.com>
2023-05-27 07:35:32 +0000

    API for State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
 '''index module'''
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
 from models.state import State
+
 
 @app_views.route('/states', strict_slashes=False)
 @app_views.route('/states/<state_id>', strict_slashes=False)
 def get_states(state_id=None):
     '''Retrieves a state'''
     if state_id is None:
-        return jsonify([s.to_dict() for s in storage.all('State').values()])
+        return jsonify([s.to_dict() for s in storage.all(State).values()])
     else:
         s = storage.get(State, state_id)
         if s:
             return jsonify(s.to_dict())
         else:
-            return jsonify({'error': 'Not found'}), 404
+            abort(404)
 
 
 @app_views.route('/states/<state_id>',
                  strict_slashes=False,
                  methods=['DELETE'])
-def delete_states(state_id=None):
+def delete_state(state_id=None):
     '''Deletes a state from the storage'''
-    if state_id is None:
-        abort(404)
     s = storage.get(State, state_id)
     if s is None:
         abort(404)
@@ -61,12 +60,10 @@ def update_state(state_id=None):
     if not kwargs:
         return jsonify({'error': 'Not a JSON'}), 400
     avoid_them = ['id', 'created_at', 'updated_at']
-    for k in avoid_them:
-        if k in kwargs:
-            del kwargs[k]
     s = storage.get(State, state_id)
     for k, v in kwargs.items():
-        setattr(s, k, v)
+        if k not in avoid_them:
+            setattr(s, k, v)
     storage.new(s)
     storage.save()
     return jsonify(s.to_dict()), 200
